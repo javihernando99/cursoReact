1643426bcf2fd6738cb003f6119bccaa7ee70cb0
Javier Hernando <javierhernando@MacBook-Pro-de-Javier.local>
2021-03-21 19:28:12 +0100

    21 de marzo se crea estructura de la calculadora

---
+++ calc-app/src/App.jsx
@@ -0,0 +1,9 @@
+//importacion 
+import React from 'react';
+// generacion de la funcion del componente
+const App = ()=> {
+    <h1>Hola mundo</h1>         
+}
+
+
+// exportacion
---
+++ calc-app/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+// importar el componente
+
+//utilizar el componente 
+ReactDom.render(<h1>Hola mundo</h1>,
+    document.getElementById('root'))
